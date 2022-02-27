1d0d7f47e40e19ce033c3cc0034f1924b42c26de
n3vo <nevo.riemer@gmail.com>
2022-02-27 21:16:46 +0200

    initialcommit

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ import Footer from "../components/Footer"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Portfolio Template" />
+    <SEO title="נבו רימר" />
     <Header></Header>
     <Work></Work>
     <Footer></Footer>
